a63cb1c2bfda4cfebbe343c50d130a7991a26d48
Uyen Hoang <minhuyen0512@gmail.com>
2021-04-10 19:28:15 -0700

    Fix base template

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('intro.html')
+    return render_template('vaccination-sites.html')
 
 @app.route('/vaccination-sites', methods=["POST", "GET"])
 def findVaccinationSites():
