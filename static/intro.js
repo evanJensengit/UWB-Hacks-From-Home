7f190b1051bc2464f961cadd4dc93ee4a803d732
Uyen Hoang <minhuyen0512@gmail.com>
2021-04-10 17:48:24 -0700

    Add base template

---
+++ static/intro.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("menu_links").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("menu_links").style.width = "0";
+  }
