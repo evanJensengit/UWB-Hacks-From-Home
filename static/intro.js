bda4274acbb9395a445b36a0fc5c3d5c1ac91d8e
mxng113 <mai.t.nguyen44@gmail.com>
2021-04-11 04:26:53 -0700

    Add jQuery and hide/show effect

--- static/intro.js
+++ static/intro.js
@@ -2,6 +2,7 @@ function openNav() {
     document.getElementById("menu_links").style.width = "250px";
   }
   
-  function closeNav() {
-    document.getElementById("menu_links").style.width = "0";
-  }
+function closeNav() {
+  document.getElementById("menu_links").style.width = "0";
+}
+
